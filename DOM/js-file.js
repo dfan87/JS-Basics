1fe61100383a0622d989632a83a2233f838f0910
dfan87 <40865929+dfan87@users.noreply.github.com>
2022-07-21 22:27:26 -0500

    updated js-file.js with div, p, h1, h3

--- DOM/js-file.js
+++ DOM/js-file.js
@@ -1,5 +1,16 @@
 const body = document.body
 const p = document.createElement("p")
+const h3 = document.createElement("h3")
+const div = document.createElement("div")
+const divh1 = document.createElement("h1")
+const p2 = document.createElement("p")
 p.textContent = "Hey I'm red!"
 p.style.color = "red"
-body.append(p)
+h3.textContent = "I'm a blue h3!"
+h3.style.color = "blue"
+div.style.borderColor = "black"
+div.style.backgroundColor = "pink"
+body.append(p, h3, div)
+divh1.textContent = "I'm in a div"
+p2.textContent = "ME TOO!"
+div.append(divh1, p2)
